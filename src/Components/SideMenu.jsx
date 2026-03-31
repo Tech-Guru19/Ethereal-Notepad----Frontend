a43776f01a1bd92ddcf5d88d936349e98672ed98
Obiasogu Esther Chizaram <chizaramesther298@gmail.com>
2026-03-31 23:53:04 +0100

    did same to SideMenu.jsx

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -13,19 +13,19 @@ import archiveIcon from "../images/archive.png"
 import closeIcon from "../images/icon-close.svg"
 
 
-const SideMenu = ({userCred, setUserCred, currentSection, setCurrentSection, setEditNote, allCollection, setCurrentCollection, setTagActive}) => {
+const SideMenu = ({ userCred, setUserCred, currentSection, setCurrentSection, setEditNote, allCollection, setCurrentCollection, setTagActive }) => {
   const [collectionOption, setCollectionOption] = useState(false)
-  const showCollections = () =>{
+  const showCollections = () => {
     setCollectionOption(!collectionOption)
   }
-  
-  const removeCollections = () =>{
+
+  const removeCollections = () => {
     setCollectionOption(false)
   }
-  const closeMenu = () =>{
-      if (window.innerWidth <= 1244) {
-          document.querySelector(".sidemenu-overall").style.display = "none"
-      }
+  const closeMenu = () => {
+    if (window.innerWidth <= 1244) {
+      document.querySelector(".sidemenu-overall").style.display = "none"
+    }
   }
 
   return (
@@ -43,51 +43,51 @@ const SideMenu = ({userCred, setUserCred, currentSection, setCurrentSection, set
       <div className="scroll-parent">
         <div className="search-parent">
           <img src={search} alt="" />
-          <input type="text" placeholder='Search'/>
+          <input type="text" placeholder='Search' />
         </div>
-        <div className="new-note" onClick={()=>{setEditNote(true),closeMenu()}}>
+        <div className="new-note" onClick={() => { setEditNote(true), closeMenu() }}>
           <img src={addIcon} alt="" />
           <p>Add New</p>
           <h6>{">"}</h6>
         </div>
         <div className="list-parent">
-          <div onClick={()=>{setCurrentSection("allNote"), setEditNote(false), removeCollections(),closeMenu()}} className={currentSection == "allNote" ? 'selected' : null}>
-              <img src={noteIcon} alt="" />
-              <p>Your Notes</p>
+          <div onClick={() => { setCurrentSection("allNote"), setEditNote(false), removeCollections(), closeMenu() }} className={currentSection == "allNote" ? 'selected' : null}>
+            <img src={noteIcon} alt="" />
+            <p>Your Notes</p>
           </div>
-          <div id='collection-overall' onClick={()=>{setEditNote(false)}} className={currentSection == "reminder"? 'selected' : null}>
-              <div onClick={showCollections}>
-                <img src={collectionIcon} alt="" />
-                <p>Collections</p>
-                <h6>{">"}</h6>
-              </div>
-              {
-                collectionOption?
+          <div id='collection-overall' onClick={() => { setEditNote(false) }} className={currentSection == "reminder" ? 'selected' : null}>
+            <div onClick={showCollections}>
+              <img src={collectionIcon} alt="" />
+              <p>Collections</p>
+              <h6>{">"}</h6>
+            </div>
+            {
+              collectionOption ?
                 <div className="collections">
                   {
-                    allCollection.map((collection)=>(
-                      <p onClick={()=>{setCurrentSection("collections"), setEditNote(false), setCurrentCollection(collection), closeMenu()}}>{collection}</p>
+                    allCollection.map((collection) => (
+                      <p onClick={() => { setCurrentSection("collections"), setEditNote(false), setCurrentCollection(collection), closeMenu() }}>{collection}</p>
                     ))
                   }
-                </div>:
+                </div> :
                 null
-              }
+            }
           </div>
-          <div onClick={()=>{setCurrentSection("reminder"), setEditNote(false), removeCollections(), closeMenu()}} className={currentSection == "reminder"? 'selected' : null}>
-              <img src={alarmIcon} alt="" />
-              <p>Reminder</p>
+          <div onClick={() => { setCurrentSection("reminder"), setEditNote(false), removeCollections(), closeMenu() }} className={currentSection == "reminder" ? 'selected' : null}>
+            <img src={alarmIcon} alt="" />
+            <p>Reminder</p>
           </div>
-          <div onClick={()=>{setTagActive(true), setEditNote(false), removeCollections(), closeMenu()}} className={currentSection == "tags"? 'selected' : null}>
-              <img src={tagIcon} alt="" />
-              <p>Tags</p>
+          <div onClick={() => { setTagActive(true), setEditNote(false), removeCollections(), closeMenu() }} className={currentSection == "tags" ? 'selected' : null}>
+            <img src={tagIcon} alt="" />
+            <p>Tags</p>
           </div>
-          <div onClick={()=>{setCurrentSection("archived"), setEditNote(false), removeCollections(), closeMenu()}} className={currentSection == "archived"? 'selected' : null}>
-              <img src={archiveIcon} alt="" />
-              <p>Archive</p>
+          <div onClick={() => { setCurrentSection("archived"), setEditNote(false), removeCollections(), closeMenu() }} className={currentSection == "archived" ? 'selected' : null}>
+            <img src={archiveIcon} alt="" />
+            <p>Archive</p>
           </div>
-          <div onClick={()=>{setCurrentSection("bin"), setEditNote(false), removeCollections(), closeMenu()}} className={currentSection == "bin"? 'selected' : null}>
-              <img src={binIcon} alt="" />
-              <p>Bin</p>
+          <div onClick={() => { setCurrentSection("bin"), setEditNote(false), removeCollections(), closeMenu() }} className={currentSection == "bin" ? 'selected' : null}>
+            <img src={binIcon} alt="" />
+            <p>Bin</p>
           </div>
         </div>
       </div>
